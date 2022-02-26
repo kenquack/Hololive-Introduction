7f16566b1c136f965b6d2d7986c3f3cacc5de583
Kenneth Quach <kennethtquach@gmail.com>
2022-02-25 18:07:43 -0800

    animations

--- src/scripts/3dscene.js
+++ src/scripts/3dscene.js
@@ -5,6 +5,8 @@ export function createScene() {
     const renderer = new THREE.WebGLRenderer();
     const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
     const scene = new THREE.Scene();
+    const mouse = new THREE.Vector2();
+    const raycaster = new THREE.Raycaster();
     let light;
 
     let Gura, Ame, Ina, Cali, Kiara;
@@ -27,51 +29,61 @@ export function createScene() {
         modelLoader.load('assets/models/smoller_gura_-_gawr_gura_holomyth/scene.gltf', (gltf) => {
             Gura = gltf.scene;
             Gura.scale.set(0.2,0.2,0.2);
-            scene.add(Gura);
             Gura.position.x = 0;
             Gura.position.y = 10;
             Gura.position.z = 15;
+            scene.add(Gura);
         });
 
         modelLoader.load('assets/models/smol_ame_-_amelia_watson_holomyth/scene.gltf', (gltf) => {
             Ame = gltf.scene;
             Ame.scale.set(0.55,0.55,0.55);
-            scene.add(Ame);
             Ame.position.x = 0.5;
             Ame.position.y = 10;
             Ame.position.z = 15;
+            scene.add(Ame);
         });
 
         modelLoader.load('assets/models/smol_ina_-_ninomae_inanis_holomyth/scene.gltf', (gltf) => {
             Ina = gltf.scene;
             Ina.scale.set(0.58,0.58,0.58);
-            scene.add(Ina);
             Ina.position.x = -0.5;
             Ina.position.y = 10;
             Ina.position.z = 15;
+            scene.add(Ina);
         });
 
         modelLoader.load('assets/models/smol_calli_-_mori_calliope_holomyth/scene.gltf', (gltf) => {
             Cali = gltf.scene;
             Cali.scale.set(0.2,0.2,0.2);
-            scene.add(Cali);
             Cali.position.x = -1;
             Cali.position.y = 10;
             Cali.position.z = 15;
+            scene.add(Cali);
         });
 
         modelLoader.load('assets/models/smol_kiara_-_takanashi_kiara_holomyth/scene.gltf', (gltf) => {
             Kiara = gltf.scene;
             Kiara.scale.set(0.55,0.55,0.55);
-            scene.add(Kiara);
             Kiara.position.x = 1;
             Kiara.position.y = 10;
             Kiara.position.z = 15;
+            scene.add(Kiara);
         });
+    }
+
+    function hoverModel() {
+        raycaster.setFromCamera(mouse, camera);
+        const intersects = raycaster.intersectObjects(scene.children);
+        for (let i = 0; i < intersects.length; i++) {
+            intersects[i].object.visible = false;
+        }
     }
 
     function animate() {
         requestAnimationFrame(animate);
+        // const mixer = new THREE.AnimationMixer(Gura); 
+        // console.log(mixer.getRoot()); <--- getting null
         rotate(Gura);
         rotate(Ina);
         rotate(Kiara);
@@ -89,6 +101,7 @@ export function createScene() {
 
 
     window.addEventListener( 'resize', onWindowResize, false );
+    window.addEventListener( 'pointermove', onPointerMove );
 
     function rotate(char) {
         if (char && char.rotation) {
@@ -103,5 +116,10 @@ export function createScene() {
     
         renderer.setSize( window.innerWidth, window.innerHeight );
     
+    };
+
+    function onPointerMove( event ) {
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
     };
 }
