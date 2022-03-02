eedcd40512051c5ac2927b23f2bb24c328affa55
Kenneth Quach <kennethtquach@gmail.com>
2022-03-02 15:49:16 -0800

    added more data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import { createScene } from "./scripts/3dscene";
 import { loadYoutubeStats } from "./scripts/youtubeAPI";
 import { createElements } from "./scripts/createElements";
 import { features } from "./scripts/features"
+import { addSliders } from "./scripts/dragScroll";
 
 createScene();
 loadYoutubeStats();
 createElements();
 features();
+addSliders();
--- src/scripts/users.js
+++ src/scripts/users.js
@@ -1,48 +1,63 @@
 export let userKeys = {
-    'Kafu': 'UCQ1U65-CQdIoZ2_NA4Z4F7A', 
+    'Gawr Gura': 'UCQ1U65-CQdIoZ2_NA4Z4F7A', 
     'Hoshimati Suisei': 'UC5CwaMl1eIgY8h02uZw7u8A',
     'Usada Pekora': 'UC1DCedRgGHBdm81E1llLhOQ',
     'Pomu Rainpuff': 'UCP4nMSTdwU1KqYWu3UH5DHQ',
-    'Nakiri Ayame': 'UC7fk0CB07ly8oSl0aqKkqFg'
+    'Nakiri Ayame': 'UC7fk0CB07ly8oSl0aqKkqFg',
+    'Kanae': 'UCspv01oxUFf_MTSipURRhkA',
+    'test2': 'UC7fk0CB07ly8oSl0aqKkqFg',
+    'test3': 'UC7fk0CB07ly8oSl0aqKkqFg',
+    'test4': 'UC7fk0CB07ly8oSl0aqKkqFg',
 };
 
 export let userIcons = {
-    'Kafu': 'assets/profile-icons/kafu-profile-icon.png',
+    'Gawr Gura': 'assets/profile-icons/gura-profile-icon.jpg',
     'Hoshimati Suisei': 'assets/profile-icons/suisei-profile-icon.jpg',
     'Usada Pekora': 'assets/profile-icons/pekora-profile-icon.jpg',
     'Pomu Rainpuff': 'assets/profile-icons/pomu-profile-icon.png',
-    'Nakiri Ayame': 'assets/profile-icons/ayame-profile-icon.png'
+    'Nakiri Ayame': 'assets/profile-icons/ayame-profile-icon.png',
+    'Kanae': 'assets/profile-icons/kanae-profile-icon.jpg',
+    'test2': 'assets/profile-icons/ayame-profile-icon.png',
+    'test3': 'assets/profile-icons/ayame-profile-icon.png',
+    'test4': 'assets/profile-icons/ayame-profile-icon.png',
 };
 
 export let userPhotos = {
-    'Kafu': 'assets/character-pics/kafu-pic.png',
+    'Gawr Gura': 'assets/character-pics/gura-pic.png',
     'Hoshimati Suisei': 'assets/character-pics/suisei-pic.png',
     'Usada Pekora': 'assets/character-pics/pekora-pic.png',
     'Pomu Rainpuff': 'assets/character-pics/pomu-pic.png',
-    'Nakiri Ayame': 'assets/character-pics/ayame-pic.png'
+    'Nakiri Ayame': 'assets/character-pics/ayame-pic.png',
+    'Kanae': 'assets/character-pics/kanae-pic.png',
+    'test2': 'assets/character-pics/ayame-pic.png',
+    'test3': 'assets/character-pics/ayame-pic.png',
+    'test4': 'assets/character-pics/ayame-pic.png'
 };
 
 export let userTwitter = {
-    'Kafu': 'https://twitter.com/virtual_kaf',
+    'Gawr Gura': 'https://twitter.com/virtual_kaf',
     'Hoshimati Suisei': 'https://twitter.com/suisei_hosimati',
     'Usada Pekora': 'https://twitter.com/usadapekora',
     'Pomu Rainpuff': 'https://twitter.com/PomuRainpuff',
-    'Nakiri Ayame': 'https://twitter.com/nakiriayame'
+    'Nakiri Ayame': 'https://twitter.com/nakiriayame',
+    'Kanae': 'https://twitter.com/Kanae_2434'
 };
 
 export let userYoutube = {
-    'Kafu': 'https://www.youtube.com/channel/UCQ1U65-CQdIoZ2_NA4Z4F7A',
+    'Gawr Gura': 'https://www.youtube.com/channel/UCoSrY_IQQVpmIRZ9Xf-y93g',
     'Hoshimati Suisei': 'https://www.youtube.com/channel/UC5CwaMl1eIgY8h02uZw7u8A',
     'Usada Pekora': 'https://www.youtube.com/channel/UC1DCedRgGHBdm81E1llLhOQ',
     'Pomu Rainpuff': 'https://www.youtube.com/channel/UCP4nMSTdwU1KqYWu3UH5DHQ',
-    'Nakiri Ayame': 'https://www.youtube.com/channel/UC7fk0CB07ly8oSl0aqKkqFg'
+    'Nakiri Ayame': 'https://www.youtube.com/channel/UC7fk0CB07ly8oSl0aqKkqFg',
+    'Kanae': 'https://www.youtube.com/channel/UCspv01oxUFf_MTSipURRhkA'
 };
 
 export let userDescription = {
-    'Kafu': 'Kafu is a Japanese female Virtual Singer on YouTube. She is a performer signed to KAMITSUBAKI STUDIO. In addition to releasing original songs, she often covers Japanese-language songs. With the concept of a virtual singer reaching out to the real world, her music videos often place her in various real life places.',
+    'Gawr Gura': 'Kafu is a Japanese female Virtual Singer on YouTube. She is a performer signed to KAMITSUBAKI STUDIO. In addition to releasing original songs, she often covers Japanese-language songs. With the concept of a virtual singer reaching out to the real world, her music videos often place her in various real life places.',
     'Hoshimati Suisei': 'Hoshimati Suisei is a Japanese virtual YouTuber. She began posting videos as an independent creator in March 2018. In May 2019, she became affiliated with Hololive Production. Her activity consists primarily of live streaming herself singing karaoke, playing video games, or talking to her fans.',
     'Usada Pekora': 'Pekora is known for being very chaotic and a prankster, someone who likes to play by her own rules. She has a unique verbal tic in which she ends her sentences with the "-peko" suffix.',
     'Pomu Rainpuff': 'Pomu is friendly, mischievous, and cheerful. She is a hardcore otaku who becomes very passionate when discussing her favorite topics, such as maid cafes or her favorite idols.',
-    'Nakiri Ayame': 'Ayame is infamous for her approachability and her rather sensitive funny bone. She streams a variety of games on her channel, like Minecraft, or FPS games such as PUBG and Apex Legends (where she ended Season 4 at Diamond 4).'
+    'Nakiri Ayame': 'Ayame is infamous for her approachability and her rather sensitive funny bone. She streams a variety of games on her channel, like Minecraft, or FPS games such as PUBG and Apex Legends (where she ended Season 4 at Diamond 4).',
+    'Kanae': 'Kanae is a self-assured, charming individual who is one of the most popular members of NIJISANJI. His popularity can be owed to his sociable personality. He is particularly good at improv and can create witty responses to conversations often. His easy-going and friendly nature allows him to hold conversations effortlessly with others.',
 };
 
