f205d3d7629299beb83fee8fe9d3834a1805c445
Kenneth Quach <kennethtquach@gmail.com>
2022-03-02 19:54:55 -0800

    more fixes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { createElements } from "./scripts/createElements";
 import { features } from "./scripts/features"
 import { addSliders } from "./scripts/dragScroll";
 
+// document.querySelector('body').style.zoom = `${1 / window.devicePixelRatio * 125}%`;
+
 createScene();
 loadYoutubeStats();
 createElements();
