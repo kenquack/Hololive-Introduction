731276f5463d9a8069e9574e2a8135fe9a94e1f7
Kenneth Quach <kennethtquach@gmail.com>
2022-03-02 17:40:13 -0800

    added more characters

--- src/scripts/users.js
+++ src/scripts/users.js
@@ -1,13 +1,13 @@
 export let userKeys = {
-    'Gawr Gura': 'UCQ1U65-CQdIoZ2_NA4Z4F7A', 
+    'Gawr Gura': 'UCoSrY_IQQVpmIRZ9Xf-y93g', 
     'Hoshimati Suisei': 'UC5CwaMl1eIgY8h02uZw7u8A',
     'Usada Pekora': 'UC1DCedRgGHBdm81E1llLhOQ',
     'Pomu Rainpuff': 'UCP4nMSTdwU1KqYWu3UH5DHQ',
     'Nakiri Ayame': 'UC7fk0CB07ly8oSl0aqKkqFg',
     'Kanae': 'UCspv01oxUFf_MTSipURRhkA',
-    'test2': 'UC7fk0CB07ly8oSl0aqKkqFg',
-    'test3': 'UC7fk0CB07ly8oSl0aqKkqFg',
-    'test4': 'UC7fk0CB07ly8oSl0aqKkqFg',
+    'Kizuna Ai': 'UC4YaOt1yT-ZeyB0OmxHgolA',
+    'Minato Aqua': 'UC1opHUrw8rvnsadT-iGp7Cg',
+    'Yukihana Lamy': 'UC7fk0CB07ly8oSl0aqKkqFg',
 };
 
 export let userIcons = {
@@ -17,9 +17,9 @@ export let userIcons = {
     'Pomu Rainpuff': 'assets/profile-icons/pomu-profile-icon.png',
     'Nakiri Ayame': 'assets/profile-icons/ayame-profile-icon.png',
     'Kanae': 'assets/profile-icons/kanae-profile-icon.jpg',
-    'test2': 'assets/profile-icons/ayame-profile-icon.png',
-    'test3': 'assets/profile-icons/ayame-profile-icon.png',
-    'test4': 'assets/profile-icons/ayame-profile-icon.png',
+    'Kizuna Ai': 'assets/profile-icons/ai-profile-icon.jpg',
+    'Minato Aqua': 'assets/profile-icons/aqua-profile-icon.jpg',
+    'Yukihana Lamy': 'assets/profile-icons/lamy-profile-icon.jpg',
 };
 
 export let userPhotos = {
@@ -29,9 +29,9 @@ export let userPhotos = {
     'Pomu Rainpuff': 'assets/character-pics/pomu-pic.png',
     'Nakiri Ayame': 'assets/character-pics/ayame-pic.png',
     'Kanae': 'assets/character-pics/kanae-pic.png',
-    'test2': 'assets/character-pics/ayame-pic.png',
-    'test3': 'assets/character-pics/ayame-pic.png',
-    'test4': 'assets/character-pics/ayame-pic.png'
+    'Kizuna Ai': 'assets/character-pics/ai-pic.png',
+    'Minato Aqua': 'assets/character-pics/aqua-pic.png',
+    'Yukihana Lamy': 'assets/character-pics/lamy-pic.png'
 };
 
 export let userTwitter = {
@@ -40,7 +40,10 @@ export let userTwitter = {
     'Usada Pekora': 'https://twitter.com/usadapekora',
     'Pomu Rainpuff': 'https://twitter.com/PomuRainpuff',
     'Nakiri Ayame': 'https://twitter.com/nakiriayame',
-    'Kanae': 'https://twitter.com/Kanae_2434'
+    'Kanae': 'https://twitter.com/Kanae_2434',
+    'Kizuna Ai': 'https://twitter.com/aichan_nel',
+    'Minato Aqua': 'https://twitter.com/minatoaqua',
+    'Yukihana Lamy': 'https://twitter.com/yukihanalamy',
 };
 
 export let userYoutube = {
@@ -49,15 +52,21 @@ export let userYoutube = {
     'Usada Pekora': 'https://www.youtube.com/channel/UC1DCedRgGHBdm81E1llLhOQ',
     'Pomu Rainpuff': 'https://www.youtube.com/channel/UCP4nMSTdwU1KqYWu3UH5DHQ',
     'Nakiri Ayame': 'https://www.youtube.com/channel/UC7fk0CB07ly8oSl0aqKkqFg',
-    'Kanae': 'https://www.youtube.com/channel/UCspv01oxUFf_MTSipURRhkA'
+    'Kanae': 'https://www.youtube.com/channel/UCspv01oxUFf_MTSipURRhkA',
+    'Kizuna Ai': 'https://www.youtube.com/channel/UC4YaOt1yT-ZeyB0OmxHgolA',
+    'Minato Aqua': 'https://www.youtube.com/channel/UC1opHUrw8rvnsadT-iGp7Cg',
+    'Yukihana Lamy': 'https://www.youtube.com/channel/UCFKOVgVbGmX65RxO3EtH3iw',
 };
 
 export let userDescription = {
-    'Gawr Gura': 'Kafu is a Japanese female Virtual Singer on YouTube. She is a performer signed to KAMITSUBAKI STUDIO. In addition to releasing original songs, she often covers Japanese-language songs. With the concept of a virtual singer reaching out to the real world, her music videos often place her in various real life places.',
+    'Gawr Gura': 'Gura is friendly and readily likeable, and often amuses her viewers with foolish antics. Gura is currently the most subscribed VTuber worldwide with over 3.8 million YouTube subscribers.',
     'Hoshimati Suisei': 'Hoshimati Suisei is a Japanese virtual YouTuber. She began posting videos as an independent creator in March 2018. In May 2019, she became affiliated with Hololive Production. Her activity consists primarily of live streaming herself singing karaoke, playing video games, or talking to her fans.',
     'Usada Pekora': 'Pekora is known for being very chaotic and a prankster, someone who likes to play by her own rules. She has a unique verbal tic in which she ends her sentences with the "-peko" suffix.',
     'Pomu Rainpuff': 'Pomu is friendly, mischievous, and cheerful. She is a hardcore otaku who becomes very passionate when discussing her favorite topics, such as maid cafes or her favorite idols.',
     'Nakiri Ayame': 'Ayame is infamous for her approachability and her rather sensitive funny bone. She streams a variety of games on her channel, like Minecraft, or FPS games such as PUBG and Apex Legends (where she ended Season 4 at Diamond 4).',
     'Kanae': 'Kanae is a self-assured, charming individual who is one of the most popular members of NIJISANJI. His popularity can be owed to his sociable personality. He is particularly good at improv and can create witty responses to conversations often. His easy-going and friendly nature allows him to hold conversations effortlessly with others.',
+    'Kizuna Ai': 'Ai is the world\'s first virtual YouTuber with the characteristics as we know today and the one who coined the term Virtual YouTuber. Ai\'s video content is similar to many other YouTubers, consisting primarily of discussions, Q&A videos, and more traditional "Let\'s Play" videos.',
+    'Minato Aqua': 'Aqua is a bumbling airhead who somehow manages to become the victim of every joke in which she gets involved. However, while Aqua appears to be a crybaby, she is deceptively skilled at highly technical video games like Sekiro, Dark Souls, Apex Legends (Diamond rank),and League of Legends (Diamond rank).',
+    'Yukihana Lamy': 'Lamy is soft-spoken, kind, and gentle, but somewhat shy and easily flustered. Lamy enjoys playing various video games such as Minecraft and ARK. Talking and drinking streams are another favorite of Lamy\'s and her drinking habits have earned her the reputation of being adrunkard.'
 };
 
